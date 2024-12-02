c39bd6e958873dce7f575d15fe35431a4ce2814c
kevster7000 <ktang147@gmail.com>
2024-12-01 20:13:18 -0600

    change cats to dogs

--- cats.js
+++ cats.js
@@ -2,9 +2,9 @@ const img = document.querySelector("#cat-img");
 const catBtn = document.querySelector("#cat-btn");
 
 async function getCatImg() {
-    const response = await fetch("https://api.thecatapi.com/v1/images/search")
+    const response = await fetch("https://dog.ceo/api/breeds/image/random")
     const newCatImg = await response.json();
-    img.src = newCatImg[0].url;
+    img.src = newCatImg.message;
 };
 
 addEventListener("DOMContentLoaded", getCatImg);
